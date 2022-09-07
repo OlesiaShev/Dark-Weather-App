2758c284daad5c1f4732e6ed5ec9061cc5924106
OlesiaShev <106448460+OlesiaShev@users.noreply.github.com>
2022-09-07 23:35:57 +0100

    Adding axios request

--- src/SearchModule.js
+++ src/SearchModule.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import background from "./images/WeatherDarkBackGroundRainbow.png";
 import SearchResults from "./SearchResults";
+import axios from "axios";
 
 export default function SearchModule() {
+  let [input, setInput] = useState("London");
+  let [name, setName] = useState("London");
+  let [temperature, setTemperature] = useState("");
+  let [description, setDescription] = useState("");
+  let [humidity, setHumidity] = useState("");
+  let [wind, setWind] = useState("");
+  let [img, setImg] = useState("");
+  let [country, setCountry] = useState("UK");
+  let forecast = [name, temperature, description, humidity, wind, img, country];
+
+  function updateValue(event) {
+    setInput(event.target.value);
+  }
+  function doSearch(event) {
+    event.preventDefault();
+    let apiKey = "4ac2c287c8855d10edca04e5759fe661";
+    let units = "metric";
+    let apiUrlByCity = `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=${units}&appid=${apiKey}`;
+
+    function showApiResponse(response) {
+      setName(response.data.name);
+      setTemperature(Math.round(response.data.main.temp));
+      setDescription(response.data.weather[0].main);
+      setHumidity(response.data.main.humidity);
+      setWind(response.data.wind.speed);
+      setImg(response.data.weather[0].icon);
+      setCountry(response.data.sys.country);
+      console.log(response);
+    }
+
+    axios.get(apiUrlByCity).then(showApiResponse);
+  }
+
   return (
     <div
       className="SearchModule container"
@@ -10,10 +44,10 @@ export default function SearchModule() {
         backgroundColor: "black",
         backgroundImage: `url(${background})`,
         backgroundRepeat: "no-repeat",
-        height: "100vh"
+        height: "100vh",
       }}
     >
-      <form>
+      <form onSubmit={doSearch}>
         <div className="row p-2 m-3">
           <div className="col-6">
             <div>
@@ -23,6 +57,7 @@ export default function SearchModule() {
                 placeholder="Enter your city"
                 autoFocus="on"
                 autoComplete="off"
+                onChange={updateValue}
               />
             </div>
           </div>
@@ -33,12 +68,12 @@ export default function SearchModule() {
           </div>
           <div className="col-3 d-flex justify-content-end">
             <a href="/" alt="use your location" className="locationSign">
-              <i class="fa-solid fa-location-crosshairs"></i>
+              <i className="fa-solid fa-location-crosshairs"></i>
             </a>
           </div>
         </div>
       </form>
-      <SearchResults />
+      <SearchResults forecast={forecast} />
     </div>
   );
 }
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -2,12 +2,21 @@ import React from "react";
 import DailyForecast from "./DailyForecast.js";
 import WeatherForecast from "./WeatherForecast.js";
 
-export default function SearchResults() {
+export default function SearchResults(props) {
+  let city = props.forecast[0];
+  let temperature = props.forecast[1];
+  let description = props.forecast[2];
+  let humidity = props.forecast[3];
+  let wind = props.forecast[4];
+  let img = props.forecast[5];
+  let country = props.forecast[6];
   return (
     <div>
       <div className="cityResults row p-2 m-3">
         <div className="col-12 col-sm-6 d-flex align-items-center">
-          <h1>London, GB</h1>
+          <h1>
+            {city}, {country}
+          </h1>
           <div className="city">
             <img
               src="http://openweathermap.org/img/wn/10d@2x.png"
