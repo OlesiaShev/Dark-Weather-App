e376c8ffc41264c78066e718c906c044158191b2
OlesiaShev <106448460+OlesiaShev@users.noreply.github.com>
2022-09-17 12:09:33 +0100

    Temperature conversion

--- src/TempCalculator.js
+++ src/TempCalculator.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function TemperatureCalculator() {
+export default function TemperatureCalculator(props) {
+  let [metric, setMetric] = useState("C");
+  let [temperature, setTemperature] = useState(props.temp);
+
+  console.log(props.temp);
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setTemperature((props.temp * 9) / 5 + 32);
+    setMetric("F");
+  }
+  function showCelcius(event) {
+    event.preventDefault();
+    setTemperature(props.temp);
+    setMetric("C");
+  }
   return (
     <div className="TemperatureCalculator">
-      <span>
+      <h5>
         {" "}
-        <a href="/" className="degreesChangeLink">
-          Show Fahrenheit
-        </a>{" "}
-        |
-        <a href="/" className="degreesChangeLink">
-          Back to Celcius
-        </a>{" "}
-      </span>
+        Current weather: {""}
+        <span> {temperature} </span>
+        <span>° {metric}</span>{" "}
+      </h5>
+      <a href="/" className="showFahrenheit" onClick={showFahrenheit}>
+        Show Fahrenheit
+      </a>{" "}
+      |
+      <a href="/" className="showCelcius" onClick={showCelcius}>
+        Back to Celcius
+      </a>{" "}
     </div>
   );
 }
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -3,14 +3,11 @@ import TemperatureCalculator from "./TempCalculator";
 
 export default function WeatherForecast(props)
 {
-  let temperature = Math.round(props.forecast.response.data.main.temp)
+  let temp = Math.round(props.forecast.response.data.main.temp)
   return (
     <div className="row p-2 m-3 WeatherForecast">
       <h5>
-        <span>Current weather: </span>
-        <span>{temperature} </span>
-        <span>° C</span>
-        <TemperatureCalculator />
+        <TemperatureCalculator temp={temp} />
         <div>
           Wind: <span>{props.forecast.response.data.wind.speed}</span> m/s
         </div>
