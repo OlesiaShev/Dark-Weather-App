f441ea2ece1f292fd7e03442c6d39c7ec5ab35ef
OlesiaShev <106448460+OlesiaShev@users.noreply.github.com>
2022-09-25 14:14:11 +0100

    Daily forecast css updating

--- src/OneDayForecast.js
+++ src/OneDayForecast.js
@@ -5,16 +5,18 @@ export default function OneDayForecast(props) {
   console.log(props);
 
   return (
-   
-      <div div className="col-4 col-md-4 p-3 ">
-        <IconsModule
-          className="iconModule"
-          iconCode={props.dailyForecast.weather[0].icon}
-          size={60}
-        />
-        <div className="box"> {props.day}</div>
-        <div>{props.dailyForecast.temp.day}</div>
-        <div>{props.dailyForecast.temp.night}</div>
+    <div div className="col-4 col-md-4 p-3 ">
+      <IconsModule
+        className="iconModule"
+        iconCode={props.dailyForecast.weather[0].icon}
+        size={60}
+      />
+          <div className="box weekDay"> { props.day }</div>
+          <div>day/night</div>
+      <div>
+        {Math.round(props.dailyForecast.temp.day)} °{" "}
+        <span>{Math.round(props.dailyForecast.temp.night)} ° </span>
       </div>
+    </div>
   );
 }
